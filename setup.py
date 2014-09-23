a2a3d24485a368ff3fd68cef63b9b4f366ef4184
Oleiade <tcrevon@gmail.com>
2014-09-23 15:53:37 +0200

    Cast setup.py long_description field type according to python version

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@ from setuptools import setup, find_packages
 import sys
 
 here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here, 'README.rst')).read()
+changes = open(os.path.join(here, 'README.rst')).read()
 
 requires = ['Django']
 if sys.version_info < (2, 7):
     requires += ['ordereddict']
+elif (3, 0) > sys.version_info >= (2, 7):
+    readme = str(readme.encode('utf-8'))
+    changes = str(changes.encode('utf-8'))
+
 
 setup(
     name='django-leaflet',
@@ -16,8 +22,7 @@ setup(
     url='https://github.com/makinacorpus/django-leaflet',
     download_url = "http://pypi.python.org/pypi/django-leaflet/",
     description="Use Leaflet in your django projects",
-    long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' +
-                     open(os.path.join(here, 'CHANGES')).read(),
+    long_description=readme + '\n\n' + changes,
     license='LPGL, see LICENSE file.',
     install_requires=requires,
     packages=find_packages(),
